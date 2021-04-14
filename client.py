0bbc78eb39ce0e5e2a8e12252f478d70d6dd4c98
Benjamin <82379366+bensnilloc@users.noreply.github.com>
2021-04-14 13:23:41 +1000

    Added potato counter to main codebase

--- client.py
+++ client.py
@@ -9,6 +9,7 @@ from classes.shop_of_offers import shop_of_offers
 from classes.trudys_surprise import trudys_surprise
 from classes.giant_jelly import giant_jelly
 from classes.kacheek_seek import kacheekseek
+from classes.potato_counter import potato_counter
 
 class client:
     def __init__(self, username, password, proxy, pin):
@@ -18,6 +19,7 @@ class client:
         self.trudys_surprise = trudys_surprise(self.wrapper, functions, username)
         self.giant_jelly = giant_jelly(self.wrapper, functions, username)
         self.kacheek_seek = kacheekseek(self.wrapper, functions, username)
+        self.potato_counter = potato_counter(self.wrapper, functions, username)
         self.functions = functions()
         self.username = username
 
@@ -67,6 +69,15 @@ class client:
             neopoints_earned = neopoints_after - neopoints_before
             self.functions.update_neopoints_gained(self.username, "kacheek seek", neopoints_earned)
             self.functions.update_last_run(self.username, "kacheek seek")
+
+    def play_potato_counter(self):
+        if int(time.time()) - self.functions.get_last_run(self.username, "potato counter") >= 86400:
+            neopoints_before = self.functions.get_neopoints_on_hand(self.wrapper.get("inventory.phtml").text)
+            self.potato_counter.play_potato_counter()
+            neopoints_after = self.functions.get_neopoints_on_hand(self.wrapper.get("inventory.phtml").text)
+            neopoints_earned = neopoints_after - neopoints_before
+            self.functions.update_neopoints_gained(self.username, "potato counter", neopoints_earned)
+            self.functions.update_last_run(self.username, "potato counter")
 
     def initiate_program(self):
         if self.wrapper.login():
